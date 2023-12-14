4c846754064ef05d39d0961567cff318653fe31f
AdrienneUsi <usiadrienne35@gmail.com>
2023-12-13 20:37:10 -0800

    did exercise2

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,33 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What is your favorite school subject?");
+            var subject = Console.ReadLine();
+
+            switch (subject)
+
+            {
+                case "Math":
+                    Console.WriteLine("Math is awesome!");
+                    break;
+                case "Science":
+                    Console.WriteLine("Science is a tough subject!");
+                    break;
+
+              default:
+                    Console.WriteLine("Ah I haven't taken that subject before");
+                    break;
+                case "English":
+                    Console.WriteLine("English is a interesting subject!");
+                    break;
+                defalut:
+                    Console.WriteLine($"Oh wow! Ihaven't taken thay subject before {subject} sounds fun"!);
+                    break;
+
+
+
+            }
+
         }
     }
 }
